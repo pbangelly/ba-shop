ec29d6c14818907ac391d49e2872a95d06391385
pbangelly <angellydejesus@gmail.com>
2022-07-26 13:15:16 -0400

    first commit

---
+++ web/components/Cart.js
@@ -0,0 +1,12 @@
+// web/components/Cart.js
+
+import { CartProvider } from "use-shopping-cart";
+import getStripe from "../lib/stripe/getStripe";
+
+export default function Cart({ children }) {
+  return (
+    <CartProvider mode="checkout-session" stripe={getStripe()} currency={"usd"}>
+      {children}
+    </CartProvider>
+  );
+}

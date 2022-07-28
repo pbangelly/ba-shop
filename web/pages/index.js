7cf0313467c2365b334d7cda15a466c0465db3ff
pbangelly <angellydejesus@gmail.com>
2022-07-28 11:58:01 -0400

    added: banner

--- web/pages/index.js
+++ web/pages/index.js
@@ -4,9 +4,9 @@ import { homeQuery } from "../lib/sanity/homeQuery";
 
 export default function Home({ posts }) {
   return (
-    <div className="w-full">
+    <div>
       <main>
-      <h1 className="bg-gradient-to-r from-indigo-600 to-fuchsia-400 text-2xl md:text-6xl text-center text-white py-10"> <a href="/">Your Bad Angel Blog</a><Link href="/merch"><a> and Shop</a></Link></h1>
+      <h1 id="banner" className="bg-gradient-to-r from-indigo-600 to-fuchsia-400 text-2xl md:text-6xl text-center text-white py-10 w-full bg-fixed"> <a href="/">Your Bad Angel Blog</a><Link href="/merch"><a> and Shop</a></Link></h1>
         <hr />
         <ul className="py-10 px-10 text-xl">
           {posts.map((p) => (

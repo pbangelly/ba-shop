cb2e1db8eccd3ba256b01982fb381d1f38cb3545
pbangelly <angellydejesus@gmail.com>
2022-09-08 08:47:15 -0400

    add: fonts

--- web/pages/index.js
+++ web/pages/index.js
@@ -9,9 +9,9 @@ export default function Home({ posts }) {
       {/* background image */}
       <div id="banner" className="absolute bottom-0 left-0 right-0 top-0 z-0 bg-center bg-norepeat bg-cover py-10">
       {/* title */}
-      <h1 className="top-0 right-0 left-0 bg-black text-red-500 text-2xl md:text-6xl text-center py-10 z-40"> <a href="/">Your Bad Angel Blog</a><Link href="/merch"><a> and Shop</a></Link></h1>
-        {/* cart */}
-        <ul className="absolute bottom-0 right-0 left-0 z-40 bg-black py-10 px-10 text-xl text-white">
+      <h1 className="top-0 right-0 left-0 font-title text-red-500 text-4xl md:text-7xl text-center py-10 px-10 z-40"> <a href="/">Your Bad Angel <br/> Blog</a><Link href="/merch"><a> and Shop</a></Link></h1>
+        {/* posts */}
+        <ul className="absolute bottom-0 right-0 left-0 z-40 py-10 px-10 text-2xl text-white font-body">
           {posts.map((p) => (
             <li key={p._id}>
               <Link href={`/posts/${p.slug}`}>
